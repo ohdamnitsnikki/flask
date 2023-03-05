4b0948c5c71a6c300c0678dd8f556312568685fc
ohdamnitsnikki <nicole_stjernberg@hotmail.com>
2023-03-05 18:40:08 +0000

    add all html files and use block elements

--- run.py
+++ run.py
@@ -8,7 +8,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello world!"  
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
+
 
     if __name__ == "__main__":
         app.run(
